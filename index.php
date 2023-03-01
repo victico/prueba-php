c0d8094c463a6b38a26ffade8b88a4530fdfb3ae
victico <frovic.ve@gmail.com>
2023-02-28 20:23:32 -0400

    arreglos en el ingreso de datos

--- index.php
+++ index.php
@@ -6,12 +6,24 @@
     echo "Hola, como estas?\nIngresa la capacidad maxima de peso del camión\nEscribe solo el numero por defecto la medida de peso es (KG):" ;
     
     $camion      = trim(fgets(STDIN)); //Peso del camion.
+    $vacas = [];
     
     //valiadamos que esta bien formateado, si pedimos que vuelva a intentar.
     if(stristr($camion, "kg") === FALSE) {
-        echo "Ingresa los datos vacas prospectos a comprar bajo el siguiente formato:\nPeso,producción - peso,producción.... (no onmitir el '-'):\n" ;
-        $vacas = trim(fgets(STDIN));
-        $vacas = explode("-", $vacas);
+        echo "\n\n\nIngresa los datos de las vacas prospectos a comprar bajo el siguiente formato:\n  Peso,producción  \nCuando hayas ingresado todo los datos escribe la palabra \"Listo\":\n" ;
+        $vaca = trim(fgets(STDIN));
+
+
+        while (strtolower($vaca) !== "listo" ) { 
+            if($vaca !=""){
+                array_push($vacas, $vaca);
+                echo "Ingrese datos de la siguiente vaca o la palabra \"listo\" si deseas terminar:\n";
+                $vaca = trim(fgets(STDIN));
+            }else{
+                echo "Datos invalidos!";
+            }
+            
+        }
         produccionTotal($camion,$vacas);
     
     }else{
